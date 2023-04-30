74c687b11e82e2237eed224093660f544593a9e9
Brian Faust <hello@basecode.sh>
2023-04-30 08:43:12 +0300

    fix: header padding on different screens

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -1,5 +1,5 @@
 <div class="lg:flex lg:h-screen lg:flex-col">
-    <header class="flex h-20 flex-none items-center justify-between border-b border-gray-200 px-6 py-4">
+    <header class="flex h-20 flex-none items-center justify-between border-b border-gray-200 px-4 py-10 sm:px-6 lg:px-8">
         <div>
             @if ($selectedView === 'day')
                 <h1 class="text-base font-semibold leading-6 text-gray-900">
