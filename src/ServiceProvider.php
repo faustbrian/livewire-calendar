d9fce0894ed35d850b9ea22887edac3e0f128ed5
Brian Faust <hello@basecode.sh>
2023-05-01 12:00:12 +0300

    feat: make remaining data classes swappable

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace BombenProdukt\LivewireCalendar;
 
 use BombenProdukt\LivewireCalendar\Data\Day;
+use BombenProdukt\LivewireCalendar\Data\DayLabel;
 use BombenProdukt\LivewireCalendar\Data\Event;
 use BombenProdukt\LivewireCalendar\Data\Month;
+use BombenProdukt\LivewireCalendar\Data\TimeLabel;
 use BombenProdukt\LivewireCalendar\Data\Week;
 use BombenProdukt\LivewireCalendar\Data\Year;
 use BombenProdukt\PackagePowerPack\Package\AbstractServiceProvider;
@@ -16,9 +18,13 @@ final class ServiceProvider extends AbstractServiceProvider
     public function packageRegistered(): void
     {
         Calendar::createEventUsing(Event::class);
+
         Calendar::createDayUsing(Day::class);
         Calendar::createMonthUsing(Month::class);
         Calendar::createWeekUsing(Week::class);
         Calendar::createYearUsing(Year::class);
+
+        Calendar::createDayLabelUsing(DayLabel::class);
+        Calendar::createTimeLabelUsing(TimeLabel::class);
     }
 }
