4c7102464c11125f742e228c969d20d47f72e587
Brian Faust <hello@basecode.sh>
2023-05-02 08:25:13 +0300

    fix: mobile switch menu

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -323,10 +323,12 @@
                             role="menuitem"
                             tabindex="-1"
                             wire:click="$set('selectedView', 'week')"
-                            @click="isOpen = false"class([ 'bg-gray-100 text-gray-900'=
-                        > $selectedView === 'week',
-                            'block w-full px-4 py-2 text-sm text-gray-700 text-left',
-                            ])>Week view</button>
+                            @click="isOpen = false"
+                            @class([
+                                'bg-gray-100 text-gray-900' => $selectedView === 'week',
+                                'block w-full px-4 py-2 text-sm text-gray-700 text-left',
+                            ])
+                        >Week view</button>
 
                         <button
                             type="button"
